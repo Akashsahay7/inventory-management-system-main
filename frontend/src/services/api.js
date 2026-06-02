56ad80fc4a765a6f24b5f64416ecd444e753b2bb
Akashsahay7 <141398977+Akashsahay7@users.noreply.github.com>
2026-06-02 10:53:10 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://inventory-management-system-production-a0d7.up.railway.app",
+  baseURL: "https://inventory-management-system-main-production.up.railway.app",
 });
 
 export default api;
